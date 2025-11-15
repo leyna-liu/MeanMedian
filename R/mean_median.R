38ef50e1e50dc8e8945981fd4f6ea611ccdc3cbe
Leyna Liu <lliu170@berkeley.edu>
2025-11-14 19:59:00 -0800

    added help file

--- R/mean_median.R
+++ R/mean_median.R
@@ -1,2 +1,8 @@
+#' Calculate mean minus median
+#'
+#' @param x A numeric vector
+#' @returns A number: the mean(x) minus median(x)
+#' @examples
+#' mean_median(c(1,2,3))
 mean_median <-
 function(x){mean(x) - median(x)}
